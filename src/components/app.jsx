47a1b64aa22c12107163f3c981bda149686f3a6c
fatalnyishod <fatalnyishod@gmail.com>
2020-06-15 22:01:38 +0300

    Личный проект: валидируй, как никогда не валидировал

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from './main.jsx';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {apartCount} = props;
+  const {apartCount, apartTitles} = props;
 
-  return <Main apartCount={apartCount} />;
+  return (
+    <Main
+      apartCount={apartCount}
+      apartTitles={apartTitles}
+    />
+  );
+};
+
+App.propTypes = {
+  apartCount: PropTypes.number.isRequired,
+  apartTitles: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
+
+const apartTitles = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+  `Wood and stone place`
+];
+
 const init = () => {
   const settings = {
     apartCount: 312
@@ -10,6 +19,7 @@ const init = () => {
   ReactDOM.render(
       <App
         apartCount={settings.apartCount}
+        apartTitles={apartTitles}
       />,
       document.getElementById(`root`)
   );
